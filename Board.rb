cda1c5c6073ef529451b543b19579adff8c3d652
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-28 14:40:34 +0200

    Override [] operator for easily get tile

--- Board.rb
+++ Board.rb
@@ -1,11 +1,17 @@
 require_relative 'Tile.rb'
 
 class Board
+    attr_reader :board
     def initialize
         @board = rand_board
     end
 
     def rand_board
         Array.new(9) {Array.new(9) {Tile.rand_tile}}
+    end
+
+    def [](pos)
+        row, col = pos
+        board[row][col]
     end
 end
