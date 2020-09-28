bec0b2088dee62a3270ad36b6487f94d032b32c6
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-28 11:53:18 +0200

    Check Tile is a bomb?

--- Tile.rb
+++ Tile.rb
@@ -1,7 +1,13 @@
 class Tile
+    attr_accessor :neighbor_bomb_count
     def initialize(value)
         @value = value
         @neighbor_bomb_count = 0
         @state = 0 # Not revealed
+    end
+
+    def bombed?
+        # Bombed if value is -1
+        value == -1
     end
 end
