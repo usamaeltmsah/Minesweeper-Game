22b93e6c192ec3feb193dc8cb0954fd6470360fc
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-28 11:55:04 +0200

    Check Tile is a flagged?

--- Tile.rb
+++ Tile.rb
@@ -1,13 +1,18 @@
 class Tile
-    attr_accessor :neighbor_bomb_count
+    attr_accessor :neighbor_bomb_count, :flagged
     def initialize(value)
         @value = value
         @neighbor_bomb_count = 0
         @state = 0 # Not revealed
+        @flagged = 0
     end
 
     def bombed?
         # Bombed if value is -1
         value == -1
+    end
+    
+    def flagged?
+        flagged == 1
     end
 end
