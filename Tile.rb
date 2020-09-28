d81fa4df235a8b462804224d34346db9bffc9157
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-28 12:05:47 +0200

    Create array for neighbours of the tile, and add neighbour method

--- Tile.rb
+++ Tile.rb
@@ -1,9 +1,11 @@
 class Tile
     attr_accessor :neighbor_bomb_count, :state
+    attr_reader :neighbors
     def initialize(value)
         @value = value
         @neighbor_bomb_count = 0
         @state = 0 # [0 => Not revealed, 1 => revealed, -1 => flagged]
+        @neighbors = []
     end
 
     def bombed?
@@ -22,4 +24,11 @@ class Tile
     def reveal
         state = 1
     end
+
+    def add_neighbour(tile_val)
+        @neighbors << tile_val
+    end
+
+    private
+    attr_writer :neighbors
 end
