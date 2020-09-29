612d5fff297c0e6d96d6b1f9d8aa64e4d06105bd
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-29 02:13:11 +0200

    Play until win or lose

--- Game.rb
+++ Game.rb
@@ -1,3 +1,5 @@
+require_relative 'Board.rb'
+
 class Minesweeper
     LAYOUTS = {
         s: { size: 9, num_bombs: 10 },
@@ -7,5 +9,23 @@ class Minesweeper
     def initialize(size)
         layout = LAYOUTS[size]
         @board = Board.new(layout[:size], layout[:num_bombs])
+    end
+
+    def play
+        until @board.won? || @board.lost?
+            puts @board.render
+            print "Enter position ex. `1 2` > "
+            pos = get_pos
+            print "Enter an action ex. `e` -> Explore or `f` -> Flag/Unflag > "
+            action = get_action
+            make_move(action, pos)
+        end
+
+        if @board.won?
+            puts "You win!"
+        elsif @board.lost?
+            puts "**LOSER!**"
+            puts @board.reveal
+        end
     end
 end
